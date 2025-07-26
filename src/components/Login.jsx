f0701b74766902c6e169423faf704c0a4cc502b0
chinmay4411 <chinmaydeshmukh12345@gmail.com>
2025-07-26 11:32:02 +0530

    Update Login.jsx

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import "./LoginSignup.css";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
-
+import { API_BASE_URL } from "../config";
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
